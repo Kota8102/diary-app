2da8a3b5ddb2f229f19ca0f241fd1daf38782dec
shoji <st.tf13@gmail.com>
2024-03-10 08:49:44 +0000

    環境変数の変数名を変更

--- src/backend/bin/backend.ts
+++ src/backend/bin/backend.ts
@@ -3,7 +3,7 @@ import * as cdk from "aws-cdk-lib";
 import { BackendStack } from "../lib/backend-stack";
 
 const app = new cdk.App();
-const targetEnv = app.node.tryGetContext("ENVIRONMENT") as string;
+const targetEnv = app.node.tryGetContext("targetEnv") as string;
 if (targetEnv == undefined) {
   throw new Error("envrionment is not set");
 }
