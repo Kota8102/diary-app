bd7655435c543436f462d57d008faafdfbc4662c
shoji <st.tf13@gmail.com>
2024-03-10 08:42:20 +0000

    デプロイ先の環境の変数名をtargetEnvに変更

--- src/backend/bin/backend.ts
+++ src/backend/bin/backend.ts
@@ -3,11 +3,11 @@ import * as cdk from "aws-cdk-lib";
 import { BackendStack } from "../lib/backend-stack";
 
 const app = new cdk.App();
-const environment = app.node.tryGetContext("ENVIRONMENT") as string;
-if (environment == undefined) {
+const targetEnv = app.node.tryGetContext("ENVIRONMENT") as string;
+if (targetEnv == undefined) {
   throw new Error("envrionment is not set");
 }
-console.log(`environment = ${environment}`);
+console.log(`environment = ${targetEnv}`);
 new BackendStack(app, "BackendStack", {
-  environment,
+  targetEnv,
 });
--- src/backend/lib/backend-stack.ts
+++ src/backend/lib/backend-stack.ts
@@ -4,18 +4,18 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 interface BackendStackProps extends cdk.StackProps {
-  environment: string;
+  targetEnv: string;
 }
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: BackendStackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, `diary-${props.environment}-bucket`, {
+    new s3.Bucket(this, `diary-${props.targetEnv}-bucket`, {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    new dynamodb.Table(this, `diary-${props.environment}-diary-db`, {
+    new dynamodb.Table(this, `diary-${props.targetEnv}-diary-db`, {
       partitionKey: {
         name: "user_id",
         type: dynamodb.AttributeType.STRING,
