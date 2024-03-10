c6c68a3c79084ec4b571fcb7420b95d75ec1c7fe
shoji <st.tf13@gmail.com>
2024-03-10 08:22:40 +0000

    TryGetContextにおけるEnvrionmentを大文字に修正

--- src/backend/bin/backend.ts
+++ src/backend/bin/backend.ts
@@ -3,7 +3,7 @@ import * as cdk from "aws-cdk-lib";
 import { BackendStack } from "../lib/backend-stack";
 
 const app = new cdk.App();
-const environment = app.node.tryGetContext("environment");
+const environment = app.node.tryGetContext("ENVIRONMENT");
 new BackendStack(app, "BackendStack", {
   environment,
 });
