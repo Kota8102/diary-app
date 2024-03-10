588d524dbc72859b11650f3c0c2f11012064f358
shoji <st.tf13@gmail.com>
2024-03-10 08:31:58 +0000

    環境がUndefinedの場合はエラーになるように変更

--- src/backend/bin/backend.ts
+++ src/backend/bin/backend.ts
@@ -3,7 +3,11 @@ import * as cdk from "aws-cdk-lib";
 import { BackendStack } from "../lib/backend-stack";
 
 const app = new cdk.App();
-const environment = app.node.tryGetContext("ENVIRONMENT");
+const environment = app.node.tryGetContext("ENVIRONMENT") as string;
+if (environment == undefined) {
+  throw new Error("envrionment is not set");
+}
+console.log(`environment = ${environment}`);
 new BackendStack(app, "BackendStack", {
   environment,
 });
