e85099813e5ae44cf5e7ab37ad232976ded4d00d
Kota8102 <52307008+Kota8102@users.noreply.github.com>
2024-03-24 15:52:02 +0900

    Feature/#36 (#48)
    cdk-nagによるセキュリティチェック導入 #36

--- src/backend/bin/backend.ts
+++ src/backend/bin/backend.ts
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { BackendStack } from "../lib/backend-stack";
+import { AwsSolutionsChecks, NagSuppressions } from 'cdk-nag';
+import { Aspects } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-const environment = app.node.tryGetContext("environment");
-new BackendStack(app, "BackendStack", {
-  environment,
-});
+
+Aspects.of(app).add(new AwsSolutionsChecks());
+
+const backendStack = new BackendStack(app, "BackendStack", {});
+
+NagSuppressions.addStackSuppressions(backendStack, [
+  {
+    id: 'AwsSolutions-COG3',
+    reason: 'このプロジェクトではAdvancedSecurityModeをENFORCEDに設定する必要はないと判断した。',
+  },
+  {
+    id: 'AwsSolutions-IAM5',
+    reason: '暫定的にオフにしているが、本番環境では適切なIAMポリシーを設定すること。',
+  }
+]);
