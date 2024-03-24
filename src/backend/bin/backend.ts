b65b11d7609cbceb625d5697f386ff03d18e62cb
Kota8102 <52307008+Kota8102@users.noreply.github.com>
2024-03-24 15:48:32 +0900

    cdk-nagを追加 (#47)
    cdk-nagを追加 #47

--- src/backend/bin/backend.ts
+++ src/backend/bin/backend.ts
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { BackendStack } from "../lib/backend-stack";
+import { AwsSolutionsChecks, NagSuppressions } from 'cdk-nag';
+import { Aspects } from 'aws-cdk-lib';
 
 const app = new cdk.App();
 
-new BackendStack(app, "BackendStack", {});
+Aspects.of(app).add(new AwsSolutionsChecks());
+
+const backendStack = new BackendStack(app, "BackendStack", {});
+
+NagSuppressions.addStackSuppressions(backendStack, [
+  {
+    id: 'AwsSolutions-COG3',
+    reason: 'このプロジェクトではAdvancedSecurityModeをENFORCEDに設定する必要はないと判断した。',
+  },
+  {
+    id: 'AwsSolutions-IAM5',
+    reason: '暫定的にオフにしているが、本番環境では適切なIAMポリシーを設定すること。',
+  }
+]);
