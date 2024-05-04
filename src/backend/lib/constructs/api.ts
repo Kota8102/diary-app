9bb4c70413db6cbc0049362ce065b3b6fea88027
Namiki Asuka <asuka0708japan@gmail.com>
2024-05-04 02:37:47 +0000

    日記を作成するLambda関数を作成しました

--- src/backend/lib/constructs/api.ts
+++ src/backend/lib/constructs/api.ts
@@ -3,11 +3,30 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { AwsSolutionsChecks, NagSuppressions } from 'cdk-nag';
 
 export class ApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const table =new dynamodb.Table(this, `diary-contents-table`, {
+      partitionKey: {
+        name: "user_id",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "date",
+        type: dynamodb.AttributeType.STRING,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      pointInTimeRecovery: true,
+    });
 
+    const diaryCreateFunction = new lambda.Function(this, 'diar-create-lambda', {
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler: 'diary_create.lambda_handler',
+      code: lambda.Code.fromAsset('lambda'),
+    });
+    table.grantWriteData(diaryCreateFunction);
     // const backendLambda = new lambda.Function(this, 'BackendLambda', {
     //   runtime: lambda.Runtime.NODEJS_14_X,
     //   code: lambda.Code.fromAsset('path/to/lambda/code'),
@@ -20,17 +39,15 @@ export class ApiStack extends cdk.Stack {
 
     // const lambdaIntegration = new apigateway.LambdaIntegration(backendLambda);
     // api.root.addMethod('GET', lambdaIntegration);
-    new dynamodb.Table(this, `DiaryContentsTable`, {
-      partitionKey: {
-        name: "user_id",
-        type: dynamodb.AttributeType.STRING,
-      },
-      sortKey: {
-        name: "date",
-        type: dynamodb.AttributeType.STRING,
-      },
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      pointInTimeRecovery: true,
-    });
+    NagSuppressions.addStackSuppressions(this, [
+      {
+        id: 'AwsSolutions-L1',
+        reason: 'CDKにより自動で作成されるLambdaに適用されてしまうため',
+       },
+       {
+        id: 'AwsSolutions-IAM4',
+        reason: 'CDKにより自動で作成されるLambdaに適用されてしまうため',
+       },
+    ])
   }
 }
