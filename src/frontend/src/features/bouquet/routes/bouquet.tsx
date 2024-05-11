209abca73e340d8c7f035c14e94cb62d130fd7ac
sfurukawa7 <shota.trp.musik711@gmail.com>
2024-05-11 02:48:17 +0000

    flower、bouquet仮ページを追加

---
+++ src/frontend/src/features/bouquet/routes/bouquet.tsx
@@ -0,0 +1,9 @@
+import { ContentLayout } from '../../../components/layout'
+
+export const Bouquet = () => {
+  return (
+    <ContentLayout>
+      <h2 className="text-lg p-4">Bouquet</h2>
+    </ContentLayout>
+  )
+}
---
+++ src/frontend/src/features/flower/routes/flower.tsx
@@ -0,0 +1,9 @@
+import { ContentLayout } from '../../../components/layout'
+
+export const Flower = () => {
+  return (
+    <ContentLayout>
+      <h2 className="text-lg p-4">Flower</h2>
+    </ContentLayout>
+  )
+}
