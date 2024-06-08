f4ef782b4aace4b38325edd994daacd22155cb05
Kota Morimoto <pontan0801.konta@gmail.com>
2024-06-09 00:11:24 +0900

    書式の調整

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { Buffer } from 'buffer'
 import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 dotenv.config();
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
+    react(),
     tsconfigPaths()
   ],
   define: {
