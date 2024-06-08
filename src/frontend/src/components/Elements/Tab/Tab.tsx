e0e709ceadf88a6c664db42865415161f6b86c25
sfurukawa7 <shota.trp.musik711@gmail.com>
2024-06-08 03:04:15 +0000

    タブメニューに文言を追加

--- src/frontend/src/components/Elements/Tab/Tab.tsx
+++ src/frontend/src/components/Elements/Tab/Tab.tsx
@@ -17,91 +17,106 @@ export const Tab = () => {
   const { pathname } = useLocation()
 
   return (
-    <div className="flex justify-around items-center h-14 bg-light-bgTab">
-      <Link to="/diarylist">
+    <div className="flex justify-around items-center bg-light-bgTab">
+      <Link
+        to="/diarylist"
+        className="flex flex-col items-center pt-2 pb-5">
         {pathname === '/diarylist' ? (
           <img
             src={calendar}
             alt="calendar"
-            width="40"
-            height="40"
+            width="32"
+            height="32"
           />
         ) : (
           <img
             src={outlinedCalendar}
             alt="outlined calendar"
-            width="40"
-            height="40"
+            width="32"
+            height="32"
           />
         )}
+        <span className="text-xs">カレンダー</span>
       </Link>
-      <Link to="/bouquet">
+      <Link
+        to="/bouquet"
+        className="flex flex-col items-center pt-2 pb-5">
         {pathname === '/bouquet' ? (
           <img
             src={bouquet}
             alt="bouquet"
-            width="40"
-            height="40"
+            width="32"
+            height="32"
           />
         ) : (
           <img
             src={outlinedBouquet}
             alt="outlined bouquet"
-            width="40"
-            height="40"
+            width="32"
+            height="32"
           />
         )}
+        <span className="text-xs">花束をみる</span>
       </Link>
-      <Link to="/flower">
+      <Link
+        to="/flower"
+        className="flex flex-col items-center pt-2 pb-5">
         {pathname === '/flower' ? (
           <img
             src={flower}
             alt="flower"
-            width="40"
-            height="40"
+            width="32"
+            height="32"
           />
         ) : (
           <img
             src={outlinedFlower}
             alt="outlined flower"
-            width="40"
-            height="40"
+            width="32"
+            height="32"
           />
         )}
+        <span className="text-xs">花をみる</span>
       </Link>
-      <Link to="/diary">
+      <Link
+        to="/diary"
+        className="flex flex-col items-center pt-2 pb-5">
         {pathname === '/diary' ? (
           <img
             src={add}
             alt="add"
-            width="40"
-            height="40"
+            width="32"
+            height="32"
           />
         ) : (
           <img
             src={outlinedAdd}
             alt="outlined add"
-            width="40"
-            height="40"
+            width="32"
+            height="32"
           />
         )}
+        <span className="text-xs">日記を追加</span>
       </Link>
-      <Link to="/setting">
+      <Link
+        to="/setting"
+        className="flex flex-col items-center pt-2 pb-5">
         {pathname === '/setting' ? (
           <img
             src={setting}
             alt="setting"
-            width="40"
-            height="40"
+            width="32"
+            height="32"
           />
         ) : (
           <img
             src={outlinedSetting}
             alt="outlined setting"
-            width="40"
-            height="40"
+            width="32"
+            height="32"
           />
         )}
+        <span className="text-xs">設定</span>
       </Link>
     </div>
   )
