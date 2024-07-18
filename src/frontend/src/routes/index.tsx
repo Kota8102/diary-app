5f87e0f8d0bd6fd00abd426d0fd619e55fc15ba4
Kota8102 <52307008+Kota8102@users.noreply.github.com>
2024-07-19 00:05:43 +0900

    ログインの有無でページが変わるように変更 (#135)

--- src/frontend/src/routes/index.tsx
+++ src/frontend/src/routes/index.tsx
@@ -1,19 +1,14 @@
 import { useRoutes } from 'react-router-dom'
 
-// import { Landing } from '../components/Elements/Landing'
-// import { useAuth } from '../features/auth/utils/cognito-auth'
+import { useAuth } from '../features/auth/utils/cognito-auth'
 
 import { protectedRoutes } from './protected'
 import { publicRoutes } from './public'
 
 export const AppRoutes = () => {
-  // const { isAuthenticated } = useAuth()
-  // const commonRoutes = [{ path: '/', element: <Landing /> }]
-  // const routes = isAuthenticated ? protectedRoutes : publicRoutes
-  // const element = useRoutes([...routes, ...commonRoutes])
-  const element = useRoutes([...protectedRoutes, ...publicRoutes])
+  const { isAuthenticated } = useAuth()
+  const routes = isAuthenticated ? protectedRoutes : publicRoutes
+  const element = useRoutes(routes)
 
-  // return <>{element}</>
   return <>{element}</>
-  
 }
