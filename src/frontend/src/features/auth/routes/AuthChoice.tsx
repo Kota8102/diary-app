76453ce937828f22ecb74a57f02d5f52e8b0bf9d
Kota Morimoto <pontan0801.konta@gmail.com>
2024-07-19 01:22:23 +0900

    デザインを微調整

--- src/frontend/src/features/auth/routes/AuthChoice.tsx
+++ src/frontend/src/features/auth/routes/AuthChoice.tsx
@@ -1,31 +1,28 @@
 import React from 'react'
 
 import logo from '../../../assets/logo.svg'
-import { AuthLayout } from '../../../components/layout'
 import { Button } from '../components/button'
 
 export const AuthChoice: React.FC = () => {
   return (
-    <AuthLayout>
-      <div className="flex flex-col justify-between items-center min-h-screen py-12 bg-light-bgDefault">
-        <div className="flex flex-grow items-center justify-center">
-          <img
-            src={logo}
-            alt="logo"
-            className="w-32 h-32"
-          />
-        </div>
-        <div className="w-full flex flex-col gap-6 px-4">
-          <Button
-            path="/auth/signup"
-            text="新規登録"
-          />
-          <Button
-            path="/auth/login"
-            text="ログイン"
-          />
-        </div>
+    <div className="flex flex-col justify-between items-center h-screen py-12 px-5">
+      <div className="flex flex-grow items-center justify-center">
+        <img
+          src={logo}
+          alt="logo"
+          className="w-32 h-32"
+        />
       </div>
-    </AuthLayout>
+      <div className="w-full flex flex-col gap-6">
+        <Button
+          path="/auth/consent"
+          text="新規登録"
+        />
+        <Button
+          path="/auth/login"
+          text="ログイン"
+        />
+      </div>
+    </div>
   )
 }
