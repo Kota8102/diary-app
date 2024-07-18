2512b77527f982097cefe5cc2672271a31a35507
Kota Morimoto <pontan0801.konta@gmail.com>
2024-07-19 01:22:34 +0900

    デザインを微調整

--- src/frontend/src/features/auth/routes/SignUp.tsx
+++ src/frontend/src/features/auth/routes/SignUp.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import { AuthLayout } from '../../../components/layout'
 import { Input } from '../components'
 import { useAuth } from '../utils/cognito-auth'
-import { useNavigate } from 'react-router-dom'
 
 export const SignUp = () => {
   const { signUp } = useAuth() // useAuth フックから signUp 関数を取得
@@ -28,36 +27,34 @@ export const SignUp = () => {
   }
 
   return (
-    <AuthLayout>
-      <div className="flex flex-col h-full p-5 gap-5">
-        <h2 className="flex items-center justify-center p-20">新規登録</h2>
-        {error && <p className="error">{error}</p>}
-        <form
-          onSubmit={handleSignUp}
-          className="space-y-7">
-          <Input
-            id="username"
-            type="text"
-            label="メールアドレス"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+    <div className="flex flex-col h-full p-5 gap-5">
+      <h2 className="flex items-center justify-center p-20">新規登録</h2>
+      {error && <p className="error">{error}</p>}
+      <form
+        onSubmit={handleSignUp}
+        className="space-y-7">
+        <Input
+          id="username"
+          type="text"
+          label="メールアドレス"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
-          <Input
-            id="password"
-            type="password"
-            label="パスワード"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="w-full bg-light-buttonPrimaryDefault p-2 rounded hover:bg-light-buttonPrimaryHover transition-colors duration-200">
-            {isLoading ? '登録中...' : 'サインアップ'}
-          </button>
-        </form>
-      </div>
-    </AuthLayout>
+        <Input
+          id="password"
+          type="password"
+          label="パスワード"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          disabled={isLoading}
+          className="w-full bg-light-buttonPrimaryDefault p-2 rounded hover:bg-light-buttonPrimaryHover transition-colors duration-200">
+          {isLoading ? '登録中...' : '新規登録'}
+        </button>
+      </form>
+    </div>
   )
 }
