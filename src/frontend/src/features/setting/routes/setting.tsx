0e742aea1b4309919b2994ce60ad170e40f5a683
Kota Morimoto <pontan0801.konta@gmail.com>
2024-07-21 15:26:23 +0900

    ログアウトを追加

--- src/frontend/src/features/setting/routes/setting.tsx
+++ src/frontend/src/features/setting/routes/setting.tsx
@@ -1,14 +1,24 @@
+import { useNavigate } from 'react-router-dom'
+
 import { ContentLayout } from '../../../components/layout'
+import { useAuth } from '../../auth/utils/cognito-auth'
 
 export const Setting = () => {
+  const { signOut } = useAuth()
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      await signOut()
+      navigate('/')
+    } catch (error) {
+      console.error('ログアウト中にエラーが発生しました:', error)
+    }
+  }
+
   return (
     <ContentLayout pagetitle="各種設定">
       <div className="flex flex-col w-full divide-y divide-gray-400">
-        <a
-          href="#"
-          className="block p-4 w-full">
-          バックアップ
-        </a>
         <a
           href="#"
           className="block p-4 w-full">
@@ -24,6 +34,11 @@ export const Setting = () => {
           className="block p-4 w-full">
           通知設定
         </a>
+        <button
+          onClick={handleLogout}
+          className="block p-4 w-full text-left">
+          ログアウト
+        </button>
         <div className="border-t border-gray-400" />
       </div>
     </ContentLayout>
