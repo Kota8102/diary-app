1e9a79c606f666c24cb1ef27416cfa0380e0e0fc
Kota Morimoto <pontan0801.konta@gmail.com>
2024-07-22 11:55:33 +0900

    cors設定を追加

--- src/backend/lambda/diary_create.py
+++ src/backend/lambda/diary_create.py
@@ -28,5 +28,11 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
+        'headers': {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         'body': json.dumps('Success')
     }
