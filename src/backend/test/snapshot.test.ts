757d2644888ae3e2e7fe95604aab0bd78cce5fa4
Kota Morimoto <pontan0801.konta@gmail.com>
2024-08-17 17:51:06 +0900

    chore: Fix vulnerabilities in pip dependencies

--- src/backend/test/snapshot.test.ts
+++ src/backend/test/snapshot.test.ts
@@ -1,17 +1,17 @@
-import { test, expect } from 'vitest'
-import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
-import { serializer } from './snapshot-plugin';
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+import { expect, test } from 'vitest'
+import { serializer } from './snapshot-plugin'
 
-import * as Backendstack from '../lib/backend-stack';
+import * as Backendstack from '../lib/backend-stack'
 
 test('Snapshot test', () => {
-  const app = new cdk.App();
+  const app = new cdk.App()
 
-  const stack = new Backendstack.BackendStack(app, 'CdkSampleStack');
+  const stack = new Backendstack.BackendStack(app, 'CdkSampleStack')
 
-  const template = Template.fromStack(stack);
+  const template = Template.fromStack(stack)
 
-  expect.addSnapshotSerializer(serializer); // シリアライザーを追加
-  expect(template).toMatchSnapshot();
-});
+  expect.addSnapshotSerializer(serializer) // シリアライザーを追加
+  expect(template).toMatchSnapshot()
+})
--- src/frontend/src/features/auth/routes/Login.tsx
+++ src/frontend/src/features/auth/routes/Login.tsx
@@ -1,45 +1,33 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import { Input } from "../components";
-import { useAuth } from "../../../lib/cognito-auth";
+import { useAuth } from '../../../lib/cognito-auth'
+import { Input } from '../components'
 
 export const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const { signIn } = useAuth();
-  const navigate = useNavigate();
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const { signIn } = useAuth()
+  const navigate = useNavigate()
 
   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(event);
-    alert(event);
-    const result = await signIn(username, password);
+    event.preventDefault()
+    console.log(event)
+    alert(event)
+    const result = await signIn(username, password)
     if (result.success) {
-      navigate("/"); // ログイン成功後のリダイレクト先
+      navigate('/') // ログイン成功後のリダイレクト先
     } else {
-      alert(result.message);
+      alert(result.message)
     }
-  };
+  }
 
   return (
     <div className="flex flex-col h-full p-5 gap-5">
       <h2 className="flex items-center justify-center p-20">ログイン</h2>
       <form onSubmit={handleLogin} className="space-y-7">
-        <Input
-          id="username"
-          label="メールアドレス"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Input
-          id="password"
-          label="パスワード"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <Input id="username" label="メールアドレス" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <Input id="password" label="パスワード" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button
           type="submit"
           className="w-full bg-light-buttonPrimaryDefault p-2 rounded hover:bg-light-buttonPrimaryHover transition-colors duration-200"
@@ -48,5 +36,5 @@ export const Login = () => {
         </button>
       </form>
     </div>
-  );
-};
+  )
+}
