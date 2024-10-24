910cf75fb4821a9f9715c3afce12c8fca6d93c1f
asuka <asuka0708japan@gmail.com>
2024-10-24 11:44:02 +0000

    change lambda object key

--- src/backend/lambda/bouquet_get/bouquet_get.py
+++ src/backend/lambda/bouquet_get/bouquet_get.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
 
     s3 = boto3.client('s3')
     bucket_name = os.environ['BUCKET_NAME']
-    key = f"{user_id}/{user_id}-{year}-{week}.png"
+    key = f"{user_id}/{year}-{week}.png"
 
     try:
         response = s3.get_object(Bucket=bucket_name, Key=key)
