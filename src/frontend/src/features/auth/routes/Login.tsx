cb37b390f0102d77906674e90462255286ee1c8d
asuka <asuka0708japan@gmail.com>
2024-11-05 13:39:33 +0000

    fix misstype

--- src/frontend/src/features/auth/routes/Login.tsx
+++ src/frontend/src/features/auth/routes/Login.tsx
@@ -18,7 +18,7 @@ export const Login = () => {
     const result = await signIn(username, password)
 
     if (result.success) {
-      navigate('/calender') // ログイン成功後のリダイレクト先
+      navigate('/calendar') // ログイン成功後のリダイレクト先
     } else {
       setErrorMessage(result.message)
     }
