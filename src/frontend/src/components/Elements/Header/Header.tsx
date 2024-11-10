2bc4f06b382649f181c7d3364ac6b690edfef429
Kota Morimoto <pontan0801.konta@gmail.com>
2024-11-10 14:19:57 +0900

    Add Header component with icon support

---
+++ src/frontend/src/components/Elements/Header/Header.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router-dom'
+
+type HeaderProps = {
+  title: string
+  showHeaderIcon: boolean
+}
+const HeaderIcon = () => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/setting')
+  }
+
+  return (
+    <button
+      type="button"
+      className="w-8 h-8 rounded-full bg-light-buttonSecondaryDefault"
+      onClick={handleClick}
+      aria-label="ブーケページへ移動"
+    />
+  )
+}
+
+
+export const Header = ({ title, showHeaderIcon }: HeaderProps) => {
+  return (
+    <div className="flex items-center justify-between w-full">
+      {title && <h1 className="text-2xl text-light-textPlaceholder">{title}</h1>}
+      {showHeaderIcon && <HeaderIcon />}
+    </div>
+  )
+}
