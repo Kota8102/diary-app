4ebf3760971626c0d5c7d6a82d2107bc829d836e
Kota Morimoto <pontan0801.konta@gmail.com>
2024-11-18 13:22:51 +0900

    feat: Add OPTIONS method to title API for CORS support

--- src/backend/lib/constructs/diary.ts
+++ src/backend/lib/constructs/diary.ts
@@ -174,5 +174,36 @@ export class Diary extends Construct {
     titleApi.addMethod('GET', new apigateway.LambdaIntegration(titleGetFunction), {
       authorizer: props.cognitoAuthorizer,
     })
+    titleApi.addMethod(
+      'OPTIONS',
+      new apigateway.MockIntegration({
+        integrationResponses: [
+          {
+            statusCode: '200',
+            responseParameters: {
+              'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+              'method.response.header.Access-Control-Allow-Origin': "'*'",
+              'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,POST,PUT,DELETE'",
+            },
+          },
+        ],
+        passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+        requestTemplates: {
+          'application/json': '{"statusCode": 200}',
+        },
+      }),
+      {
+        methodResponses: [
+          {
+            statusCode: '200',
+            responseParameters: {
+              'method.response.header.Access-Control-Allow-Headers': true,
+              'method.response.header.Access-Control-Allow-Origin': true,
+              'method.response.header.Access-Control-Allow-Methods': true,
+            },
+          },
+        ],
+      },
+    )
   }
 }
