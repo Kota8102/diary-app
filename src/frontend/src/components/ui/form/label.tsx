31f1979e4e9fea44a5e472afcb25950691ee382a
Konta <52307008+Kota8102@users.noreply.github.com>
2024-11-18 11:48:45 +0900

    feat: Add diary creation and flower display components  (#236)

--- src/frontend/src/components/ui/form/label.tsx
+++ src/frontend/src/components/ui/form/label.tsx
@@ -4,12 +4,20 @@ import * as React from 'react'
 
 import { cn } from '@/utils/cn'
 
+// ラベルのバリエーション
 const labelVariants = cva('text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70')
 
+// Labelコンポーネントの定義
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />)
+>(
+  // LabelPrimitive.Rootを基にしたLabelコンポーネントを作成
+  ({ className, ...props }, ref) => <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />,
+)
+
+// Labelコンポーネントの表示名を設定
 Label.displayName = LabelPrimitive.Root.displayName
 
+// Labelコンポーネントをエクスポート
 export { Label }
