8d1e57e3c34bf3ccdb1895d2460046fc85687c38
Kota Morimoto <pontan0801.konta@gmail.com>
2024-11-18 11:24:52 +0900

    Refactor 日記のタイトル型 and 日記の内容型 in api.ts

--- src/frontend/src/types/api.ts
+++ src/frontend/src/types/api.ts
@@ -6,14 +6,12 @@ export type Entity<T> = {
   [K in keyof T]: T[K]
 }
 
+// 日記のタイトル型
 export type Title = Entity<{
   title: string
 }>
 
-// export type Note = Entity<{
-//   note: string
-// }>
-
+// 日記の内容型
 export type Note = Entity<{
   updated_at: string
   content: string
@@ -22,4 +20,11 @@ export type Note = Entity<{
   created_at: string
   diary_id: string
   is_deleted: boolean
+}>
+
+// 日記の作成入力型
+export type CreateDiary = Entity<{
+  message: string
+  flower_id: string
+  flower_image: string
 }>
