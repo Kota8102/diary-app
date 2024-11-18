fa633bd66b0beb04d43a6dba5c74e49f2fa87d23
Kota Morimoto <pontan0801.konta@gmail.com>
2024-11-18 11:26:04 +0900

    Add route for displaying flowers in diary

--- src/frontend/src/app/routes/app/diary/diary-create.tsx
+++ src/frontend/src/app/routes/app/diary/diary-create.tsx
@@ -1,72 +1,72 @@
-import moment from 'moment'
-import { useEffect, useState } from 'react'
-import Datetime from 'react-datetime'
-import { useNavigate, useParams } from 'react-router-dom'
-
-import { DisabledButton } from '@/components/Elements/Button'
-import { ContentLayout } from '@/components/layout/content-layout'
-import { useCreateDiary } from '@/features/writeDiary/api/create-diary'
-
-import '@/features/writeDiary/styles/react-datetime.css'
+import { ContentLayout } from '@/components/layout/'
+import { DiaryCreate } from '@/features/diary/componets/diary-create'
 
 export const DiaryCreateRoute = () => {
-  const { date } = useParams<{ date: string }>()
-  const navigate = useNavigate()
-
-  const [diary, setDiary] = useState('')
-  const [currentDate, setCurrentDate] = useState(date || moment().format('YYYY-MM-DD'))
-
-  const createDiaryMutation = useCreateDiary()
-
-  // yyyy-mm-dd以外の日付が入力された場合、トップページにリダイレクト
-  useEffect(() => {
-    const datePattern = /^\d{4}-\d{2}-\d{2}$/
-    if (!datePattern.test(currentDate)) {
-      navigate('/')
-    }
-  }, [currentDate, navigate])
-
-  const handleDateChange = (newDate: moment.Moment | string) => {
-    const formattedDate = moment(newDate).format('YYYY-MM-DD')
-    setCurrentDate(formattedDate)
-    navigate(`/diary/${formattedDate}`)
-  }
-
-  const handleSubmit = () => {
-    createDiaryMutation.mutate(
-      { date: currentDate, content: diary },
-      {
-        onSuccess: () => {
-          alert('日記が正常に作成されました。')
-          setDiary('') // 入力フィールドをクリア
-        },
-        onError: (error) => {
-          console.error('日記の作成に失敗しました:', error)
-          alert('日記の作成に失敗しました。もう一度お試しください。')
-        },
-      },
-    )
-  }
-
   return (
     <ContentLayout pagetitle="Diary">
-      <div className="flex flex-col w-full h-full gap-5">
-        <Datetime value={moment(currentDate, 'YYYY-MM-DD')} dateFormat={'YYYY / MM / DD'} timeFormat={false} onChange={handleDateChange} />
-        <textarea
-          className="flex-grow w-full p-4 rounded-md bg-light-bgText resize-none"
-          placeholder="ここに日記を入力してください。"
-          value={diary}
-          onChange={(e) => setDiary(e.target.value)}
-        />
-        <DisabledButton
-          text={createDiaryMutation.isPending ? '生成中...' : '生成する'}
-          onClick={handleSubmit}
-          css=""
-          disabled={diary.trim() === '' || createDiaryMutation.isPending}
-          enabledCss="bg-light-buttonPrimaryDefault hover:bg-light-buttonPrimaryHover"
-          disabledCss="bg-light-buttonPrimaryDisabled"
-        />
-      </div>
+      <DiaryCreate />
     </ContentLayout>
   )
 }
+
+// export const DiaryCreateRoute = () => {
+//   const { date } = useParams<{ date: string }>()
+//   const navigate = useNavigate()
+
+//   const [diary, setDiary] = useState('')
+//   const [currentDate, setCurrentDate] = useState(date || moment().format('YYYY-MM-DD'))
+
+//   const createDiaryMutation = useCreateDiary()
+
+//   // yyyy-mm-dd以外の日付が入力された場合、トップページにリダイレクト
+//   useEffect(() => {
+//     const datePattern = /^\d{4}-\d{2}-\d{2}$/
+//     if (!datePattern.test(currentDate)) {
+//       navigate('/')
+//     }
+//   }, [currentDate, navigate])
+
+//   const handleDateChange = (newDate: moment.Moment | string) => {
+//     const formattedDate = moment(newDate).format('YYYY-MM-DD')
+//     setCurrentDate(formattedDate)
+//     navigate(`/diary/${formattedDate}`)
+//   }
+
+//   const handleSubmit = () => {
+//     createDiaryMutation.mutate(
+//       { date: currentDate, content: diary },
+//       {
+//         onSuccess: () => {
+//           alert('日記が正常に作成されました。')
+//           setDiary('') // 入力フィールドをクリア
+//         },
+//         onError: (error) => {
+//           console.error('日記の作成に失敗しました:', error)
+//           alert('日記の作成に失敗しました。もう一度お試しください。')
+//         },
+//       },
+//     )
+//   }
+
+//   return (
+//     <ContentLayout pagetitle="Diary">
+//       <div className="flex flex-col w-full h-full gap-5">
+//         <Datetime value={moment(currentDate, 'YYYY-MM-DD')} dateFormat={'YYYY / MM / DD'} timeFormat={false} onChange={handleDateChange} />
+//         <textarea
+//           className="flex-grow w-full p-4 rounded-md bg-light-bgText resize-none"
+//           placeholder="ここに日記を入力してください。"
+//           value={diary}
+//           onChange={(e) => setDiary(e.target.value)}
+//         />
+//         <DisabledButton
+//           text={createDiaryMutation.isPending ? '生成中...' : '生成する'}
+//           onClick={handleSubmit}
+//           css=""
+//           disabled={diary.trim() === '' || createDiaryMutation.isPending}
+//           enabledCss="bg-light-buttonPrimaryDefault hover:bg-light-buttonPrimaryHover"
+//           disabledCss="bg-light-buttonPrimaryDisabled"
+//         />
+//       </div>
+//     </ContentLayout>
+//   )
+// }
---
+++ src/frontend/src/features/diary/componets/display-flower.tsx
@@ -0,0 +1,39 @@
+import { Button } from '@/components/ui/button/button'
+import { paths } from '@/config/paths'
+import { useNavigate, useParams } from 'react-router-dom'
+
+export type DisplayFlowerProps = {
+  image: string
+}
+
+export const DisplayFlower = ({ image }: DisplayFlowerProps) => {
+  const navigate = useNavigate()
+
+  const { date } = useParams<{ date: string }>()
+
+  // 花の表示ページに遷移
+  const handleClick = () => {
+    navigate(paths.app.flower.getHref(date ?? ''))
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full pb-14">
+      <div className="flex flex-col items-center justify-center flex-grow">
+        <img
+          className=""
+          src={`data:image/jpeg;base64,${image}`}
+          alt="Flower"
+          style={{ maxWidth: '100%', height: 'auto' }}
+          onError={(e) => {
+            console.error('画像の読み込みに失敗しました')
+            e.currentTarget.style.display = 'none'
+          }}
+        />
+      </div>
+      <div className="w-full mt-auto">
+        <div className="text-center text-xl text-light-textDefault font-medium pb-16">花が生成されました！</div>
+        <Button onClick={handleClick}>表示する</Button>
+      </div>
+    </div>
+  )
+}
