5bb4182158ae183d257700864bc98d119f4a7067
shoji <st.tf13@gmail.com>
2024-12-08 01:43:52 +0000

    プロフィール画像アップロード用CDKファイル作成

---
+++ src/backend/lib/constructs/settings.ts
@@ -0,0 +1,53 @@
+import * as cdk from 'aws-cdk-lib'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
+import type * as cognito from 'aws-cdk-lib/aws-cognito'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import { Construct } from 'constructs'
+
+export interface BouquetProps {
+  userPool: cognito.UserPool
+  api: apigateway.RestApi
+  cognitoAuthorizer: apigateway.CognitoUserPoolsAuthorizer
+}
+
+export class Bouquet extends Construct {
+  constructor(scope: Construct, id: string, props: BouquetProps) {
+    super(scope, id)
+
+    // 花束画像保存用のS3バケットの作成
+    const userSettingsBucket = new s3.Bucket(this, 'userSettingsBucket', {
+      enforceSSL: true,
+      serverAccessLogsPrefix: 'log/',
+    })
+
+    //プロフィール画像アップロード用Lambda関数の定義
+    const uploadProfileImageFunction = new lambda.Function(this, 'uploadProfileImageFunction', {
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler: 'upload_profile_image_function.lambda_handler',
+      timeout: cdk.Duration.seconds(15),
+      code: lambda.Code.fromAsset('lambda/upload_profile_image_function', {
+        bundling: {
+          image: lambda.Runtime.PYTHON_3_11.bundlingImage,
+        },
+      }),
+      environment: {
+        USER_SETTINGS_BUCKET: userSettingsBucket.bucketName,
+      },
+    })
+
+    // /APIの設定
+    const settingsApi = props.api.root.addResource('settings')
+
+    settingsApi.addMethod('POST', new apigateway.LambdaIntegration(uploadProfileImageFunction), {
+      requestParameters: {
+        'method.request.querystring.date': true,
+      },
+      requestValidatorOptions: {
+        requestValidatorName: 'ValidateQueryString',
+        validateRequestParameters: true,
+      },
+      authorizer: props.cognitoAuthorizer,
+    })
+  }
+}
