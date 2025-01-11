ec31a82c6af3bd8b873a68b6e360e0f1914568fd
asuka <asuka0708japan@gmail.com>
2025-01-11 03:41:23 +0000

    modify test_diary_create

--- src/backend/test/pytest/test_diary_create.py
+++ src/backend/test/pytest/test_diary_create.py
@@ -89,7 +89,7 @@ def test_invoke_flower_lambda(mock_boto_client):
 @pytest.fixture
 def mock_env():
     original_env = dict(os.environ)
-    os.environ["FLOWER_IMAGE_BUCKET_NAME"] = "test-bucket"
+    os.environ["ORIGINAL_IMAGE_BUCKET_NAME"] = "test-bucket"
     yield
     os.environ.clear()
     os.environ.update(original_env)
