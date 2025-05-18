3264e915860abb699816fda5140ecb260cdde2a0
asuka <asuka0708japan@gmail.com>
2025-05-18 13:09:47 +0000

    fix biome check

--- src/frontend/src/features/diary/components/display-bouquet.tsx
+++ src/frontend/src/features/diary/components/display-bouquet.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button/button'
 import { paths } from '@/config/paths'
-import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export type DisplayBouquetProps = {
   image: string
